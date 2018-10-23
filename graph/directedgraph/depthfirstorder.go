5a256ae84d70e87c0500b063ec46165518aaa1dc
yqsy <yqsy021@126.com>
2018-10-23 17:17:21 +0800

    顶点排序

---
+++ graph/directedgraph/depthfirstorder.go
@@ -0,0 +1,41 @@
+package directedgraph
+
+import (
+	"github.com/golang-collections/collections/queue"
+	"github.com/golang-collections/collections/stack"
+)
+
+type DepthFirstOrder struct {
+	marked      []bool
+	pre         *queue.Queue // 所有顶点的前序排列
+	post        *queue.Queue // 所有顶点的后续排列
+	reversePost *stack.Stack // 所有顶点的逆后续排序
+}
+
+func NewDepthFirstOrder(g *Digraph) *DepthFirstOrder {
+	d := &DepthFirstOrder{}
+	d.pre = queue.New()
+	d.post = queue.New()
+	d.reversePost = stack.New()
+	d.marked = make([]bool, g.V)
+	for v := 0; v < g.V; v++ {
+		if !d.marked[v] {
+			d.dfs(g, v)
+		}
+	}
+	return d
+}
+
+func (d *DepthFirstOrder) dfs(g *Digraph, v int) {
+	d.pre.Enqueue(v)
+
+	d.marked[v] = true
+	for _, w := range g.adj[v] {
+		if !d.marked[w] {
+			d.dfs(g, w)
+		}
+	}
+
+	d.post.Enqueue(v)
+	d.reversePost.Push(v)
+}
--- graph/directedgraph/directedgraph_test.go
+++ graph/directedgraph/directedgraph_test.go
@@ -58,9 +58,47 @@ func TestDirectedCycle(t *testing.T) {
 	r := bufio.NewReader(f)
 	g := NewDigraphFromBufio(r)
 
-
 	d := NewDirectedCycle(g)
 
 	fmt.Println(d.HasCycle())
 }
 
+// 基于深度优先搜索的顶点排序
+func TestDepthFirstOrder(t *testing.T) {
+
+	f, err := os.Open("tinyG.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	g := NewDigraphFromBufio(r)
+
+	d := NewDepthFirstOrder(g)
+
+	length := d.pre.Len()
+	for i := 0; i < length; i++ {
+		ele := d.pre.Dequeue().(int)
+		fmt.Printf("%v ", ele)
+	}
+	fmt.Println()
+
+	length = d.post.Len()
+	for i := 0; i < length; i++ {
+		ele := d.post.Dequeue().(int)
+		fmt.Printf("%v ", ele)
+	}
+	fmt.Println()
+
+
+	length = d.reversePost.Len()
+	for i := 0; i < length; i++ {
+		ele := d.reversePost.Pop().(int)
+		fmt.Printf("%v ", ele)
+	}
+	fmt.Println()
+
+
+}
